6350241014b7745716fea1848786688634f82b15
kjha <77478989+jjcszxh@users.noreply.github.com>
2026-03-15 11:13:07 +0800

    Update epg.go
    1.增加结构体参数

--- config/epg.go
+++ config/epg.go
@@ -4,5 +4,7 @@ type Epg struct {
 	Generator string `mapstructure:"generator" json:"generator" yaml:"generator"`
 	Source    string `mapstructure:"source" json:"source" yaml:"source"`
 	XmlUrl    string `mapstructure:"xml_url" json:"xml_url" yaml:"xml_url"`
+	RtspUrl   string `mapstructure:"rtsp_url" json:"rtsp_url" yaml:"rtsp_url"`
+	RtpUrl    string `mapstructure:"rtp_url" json:"rtp_url" yaml:"rtp_url"`
 	FetchCron string `mapstructure:"fetch_cron" json:"fetch_cron" yaml:"fetch_cron"`
 }
